86f2af0e1e6d877f1529fd75955438a16bda7bcc
jerry <jinyu.gu1997uiuc@gmail.com>
2021-09-28 15:41:01 +0900

    cfg update

--- config/config_param.py
+++ config/config_param.py
@@ -1,3 +1,8 @@
-
+import numpy as np
 
 class_names = ['Car', 'Person', 'Cyclist']
+
+anchors = np.array([(27, 23), (32, 30), (55, 38),
+                    (76, 52), (81, 65), (138, 44),
+                    (116, 115), (190, 114), (309, 174)],
+                   np.float32)
